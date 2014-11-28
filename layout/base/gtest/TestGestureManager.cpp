f8d987541d14454a7dc97057e1d9a3868356cc0f
Morris Tseng <mtseng@mozilla.com>
2014-11-28 18:20:16 +0800

    First gtest!!! type following command to execute gtest. ./mach gtest "GestureManager*"

--- layout/base/gtest/TestGestureManager.cpp
+++ layout/base/gtest/TestGestureManager.cpp
@@ -7,14 +7,61 @@
 #include "gmock/gmock.h"
 
 #include "CopyPasteManager.h"
+#include "gfxPrefs.h"
+#include "mozilla/BasicEvents.h"
+#include "mozilla/MouseEvents.h"
 
 using namespace mozilla;
+using ::testing::_;
+using ::testing::AtLeast;
+using ::testing::DefaultValue;
 
-class MockCopyPasteManager : CopyPasteManager
+class MockCopyPasteManager : public CopyPasteManager
 {
+public:
   MOCK_METHOD1(OnPress, nsEventStatus(const nsPoint&));
   MOCK_METHOD1(OnDrag, nsEventStatus(const nsPoint&));
   MOCK_METHOD0(OnRelease, nsEventStatus());
   MOCK_METHOD1(OnLongTap, nsEventStatus(const nsPoint&));
   MOCK_METHOD1(OnTap, nsEventStatus(const nsPoint&));
 };
+
+class MockGestureManager : public CopyPasteManager::GestureManager {
+public:
+  MockGestureManager(MockCopyPasteManager* aManager)
+    : CopyPasteManager::GestureManager(nullptr, aManager)
+  { }
+
+  CopyPasteManager::GestureManager::InputType GetType() { return mType; }
+  CopyPasteManager::GestureManager::InputState GetState() { return mState; }
+
+  typedef CopyPasteManager::GestureManager::InputState InputState;
+  typedef CopyPasteManager::GestureManager::InputType InputType;
+};
+
+class GestureManagerTester : public ::testing::Test {
+protected:
+  virtual void SetUp()
+  {
+    gfxPrefs::GetSingleton();
+    mMockHandler = new MockCopyPasteManager();
+    mGestureManager = new MockGestureManager(mMockHandler);
+
+    DefaultValue<nsEventStatus>::Set(nsEventStatus_eIgnore);
+  }
+
+  nsRefPtr<MockGestureManager> mGestureManager;
+  nsRefPtr<MockCopyPasteManager> mMockHandler;
+};
+
+TEST_F(GestureManagerTester, TestOnPress) {
+  EXPECT_CALL(*mMockHandler, OnPress(_))
+    .Times(AtLeast(1));
+
+  WidgetMouseEvent evt(true, NS_MOUSE_BUTTON_DOWN, nullptr, WidgetMouseEvent::eReal);
+  evt.button = WidgetMouseEvent::eLeftButton;
+  mGestureManager->HandleEvent(&evt);
+
+  EXPECT_EQ(mGestureManager->GetState(), MockGestureManager::InputState::PRESS);
+  EXPECT_EQ(mGestureManager->GetType(), MockGestureManager::InputType::MOUSE);
+}
