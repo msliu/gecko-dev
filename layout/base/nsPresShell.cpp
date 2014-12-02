44a8c84b94d42849ceaa4a0fa2a5c3080b7fac13
Morris Tseng <mtseng@mozilla.com>
2014-12-02 14:11:35 +0800

    Merge glue code back to CopyPasteManager.

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -918,7 +918,7 @@ PresShell::Init(nsIDocument* aDocument,
   }
 
   if (AccessibleCaretEnabled()) {
-    mCopyPasteManager = new CopyPasteManager();
+    mCopyPasteManager = new CopyPasteManager(this);
   }
 
   NS_ADDREF(mSelection = new nsFrameSelection());
@@ -1939,7 +1939,7 @@ PresShell::Initialize(nscoord aWidth, nscoord aHeight)
 
     // Initialize after nsCanvasFrame is created.
     if (mCopyPasteManager) {
-      mCopyPasteManager->Init(this);
+      mCopyPasteManager->Init();
     }
 
     // nsAutoScriptBlocker going out of scope may have killed us too
